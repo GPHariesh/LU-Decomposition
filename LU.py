72c1fca824b02f764235f0e111f8cfbc5f52c9f0
GPHariesh <harieshgp@gmail.com>
2025-04-19 21:32:06 +0530

    added

--- LU.py
+++ LU.py
@@ -1,14 +1,16 @@
-# To print L and U matrix
 import numpy as np
-#add the library to import lu
+from scipy.linalg import lu
 A = np.array(eval(input()))
-#add the code to get the L and U matrix
+P,L,U=lu(A)
+print(L)
+print(U)
 
 
-# To print X matrix (solution to the equations)
 import numpy as np
-#add the library to import lu_factor, lu_solve
+from scipy.linalg import lu_factor,lu_solve
 A = np.array(eval(input()))
-b = np.array(eval(input()))
-#add the code to get the solution of the matrix
+B = np.array(eval(input()))
+piv,lu=lu_factor(A)
+result=lu_solve((piv,lu),B)
+print(result)
 
